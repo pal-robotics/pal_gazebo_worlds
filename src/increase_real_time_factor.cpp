dd591b81b81090fe4e8de5e4ac53bfbda1a289b2
Noel Jimenez <noel.jimenez@pal-robotics.com>
2022-09-19 08:41:16 +0200

    update copyright and license

--- src/increase_real_time_factor.cpp
+++ src/increase_real_time_factor.cpp
@@ -1,11 +1,16 @@
-/*
- * Copyright 2019 PAL Robotics SL. All Rights Reserved
- *
- * Unauthorized copying of this file, via any medium is strictly prohibited,
- * unless it was supplied under the terms of a license agreement or
- * nondisclosure agreement with PAL Robotics SL. In this case it may not be
- * copied or disclosed except in accordance with the terms of that agreement.
- */
+// Copyright (c) 2022 PAL Robotics S.L. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <ros/ros.h>
 #include <gazebo_msgs/GetPhysicsProperties.h>
